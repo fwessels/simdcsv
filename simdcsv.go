fbfa43caf9fd002e41257e76db87daca9b4f1a17
frankw <fwessels@xs4all.nl>
2020-02-10 16:59:05 -0800

    Initial commit

---
+++ simdcsv.go
@@ -0,0 +1,31 @@
+package simdcsv
+
+import (
+	"fmt"
+)
+
+func stage1(line []byte) []uint32 {
+
+	return []uint32{0xcf, 10, 1, 19, 1, 4, 1, 0, 1, 0, 1, 2, 1, 6, 1, 0, 1, 4, 1, 2, 1, 2, 1, 15, 1, 5, 1, 1, 1, 6, 1, 18, 1, 2, 1, 5, 1, 5,
+		               2, 10, 1, 19, 1, 4, 1, 0, 1, 0, 1, 2, 1, 6, 1, 0, 1, 3, 1, 2, 1, 2, 1, 13, 1, 4, 1, 1, 1, 6, 1, 18, 1, 2, 1, 5, 1, 5}
+}
+
+func stage2(buf []byte, incrs []uint32) {
+
+	const columns = 19
+
+	offset, end := uint64(0), uint64(0)
+	for i := 0; i < len(incrs); i += 2 {
+
+		offset += uint64(incrs[i])
+		end = offset + uint64(incrs[i+1])
+		if end > offset {
+			fmt.Print("'" + string(buf[offset:end]) + "'", " ")
+			offset = end
+		}
+
+		if i % (columns*2) == 18*2 {
+			fmt.Println()
+		}
+	}
+}
---
+++ simdcsv_test.go
@@ -0,0 +1,24 @@
+package simdcsv
+
+import (
+	"testing"
+	"io/ioutil"
+)
+
+func TestCsv(t *testing.T) {
+
+	buf, _ := ioutil.ReadFile("parking-citations-10.csv")
+
+	incrs := stage1(buf)
+	stage2(buf, incrs)
+}
+
+func TestCsvSpaces(t *testing.T) {
+
+	// benchmark approach with spaces
+}
+
+func TestCsvEscapes(t *testing.T) {
+
+	// benchmark approach with escaped fields
+}
