a30d94a905800961e87535321aea5fb11c3f89bb
frankw <fwessels@xs4all.nl>
2020-03-13 14:23:29 -0700

    Add test case for tab character

--- find_separator_amd64_test.go
+++ find_separator_amd64_test.go
@@ -2,6 +2,7 @@ package simdcsv
 
 import (
 	"testing"
+	"strings"
 )
 
 func TestFindSeparator(t *testing.T) {
@@ -23,6 +24,26 @@ func TestFindSeparator(t *testing.T) {
 
 		if mask != tc.expected {
 			t.Errorf("TestFindSeparator(%d): got: 0x%x want: 0x%x", i, mask, tc.expected)
+		}
+	}
+}
+
+func TestFindSeparatorTab(t *testing.T) {
+
+	testCases := []struct {
+		input       string
+		expected    uint64
+	}{
+		{strings.Repeat("\t", 64), ^uint64(0x0)},
+		{"column1\tcolumn2\tcolumn3\tcolumn4\tcolumn5\tcolumn6\tcolumn7\tcolumn8\t", 0x8080808080808080},
+	}
+
+	for i, tc := range testCases {
+
+		mask := find_separator([]byte(tc.input), 0x9)
+
+		if mask != tc.expected {
+			t.Errorf("TestFindSeparatorTab(%d): got: 0x%x want: 0x%x", i, mask, tc.expected)
 		}
 	}
 }
