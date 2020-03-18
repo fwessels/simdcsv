8df6f38aa038edee5634c7a1f806e789b1258977
frankw <fwessels@xs4all.nl>
2020-03-18 14:36:38 -0700

    Initial code for benchmarking Stage 1

--- stage1_find_marks_amd64_test.go
+++ stage1_find_marks_amd64_test.go
@@ -160,3 +160,37 @@ func TestStage1DetectDoubleQuotes(t *testing.T) {
 		fmt.Println(quoteBits)
 	}
 }
+
+func BenchmarkStage1(b *testing.B) {
+
+	vectors := []string{
+		"1103341116,2015-12-21T00:00:00,1251,,,CA,200304,,HOND,PA,GY,13147 WELBY WAY,01521,1,4000A1,NO EVIDENCE OF REG,50,99999,99999    ",
+		"1103700150,2015-12-21T00:00:00,1435,,,CA,201512,,GMC,VN,WH,525 S MAIN ST,1C51,1,4000A1,NO EVIDENCE OF REG,50,99999,99999        ",
+		"1104803000,2015-12-21T00:00:00,2055,,,CA,201503,,NISS,PA,BK,200 WORLD WAY,2R2,2,8939,WHITE CURB,58,6439997.9,1802686.4          ",
+		"1104820732,2015-12-26T00:00:00,1515,,,CA,,,ACUR,PA,WH,100 WORLD WAY,2F11,2,000,17104h,,6440041.1,1802686.2                      ",
+		"1105461453,2015-09-15T00:00:00,115,,,CA,200316,,CHEV,PA,BK,GEORGIA ST/OLYMPIC,1FB70,1,8069A,NO STOPPING/STANDING,93,99999,99999 ",
+		"1106226590,2015-09-15T00:00:00,19,,,CA,201507,,CHEV,VN,GY,SAN PEDRO S/O BOYD,1A35W,1,4000A1,NO EVIDENCE OF REG,50,99999,99999   ",
+		"1106500452,2015-12-17T00:00:00,1710,,,CA,201605,,MAZD,PA,BL,SUNSET/ALVARADO,00217,1,8070,PARK IN GRID LOCK ZN,163,99999,99999   ",
+		"1106500463,2015-12-17T00:00:00,1710,,,CA,201602,,TOYO,PA,BK,SUNSET/ALVARADO,00217,1,8070,PARK IN GRID LOCK ZN,163,99999,99999   ",
+		"1106506402,2015-12-22T00:00:00,945,,,CA,201605,,CHEV,PA,BR,721 S WESTLAKE,2A75,1,8069AA,NO STOP/STAND AM,93,99999,99999         ",
+		"1106506413,2015-12-22T00:00:00,1100,,,CA,201701,,NISS,PA,SI,1159 HUNTLEY DR,2A75,1,8069AA,NO STOP/STAND AM,93,99999,99999       ",
+	}
+
+	b.SetBytes(int64(len(vectors)*128))
+	b.ReportAllocs()
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		for _, v :=  range vectors {
+
+			find_separator([]byte(v), ',')
+			find_separator([]byte(v[64:]), ',')
+
+			prev_iter_inside_quote, error_mask := uint64(0), uint64(0)
+			qb1, qb2 := uint64(0), uint64(0)
+
+			find_quote_mask_and_bits([]byte(v), 0, &prev_iter_inside_quote, &qb1, &error_mask)
+			find_quote_mask_and_bits([]byte(v[64:]), 0, &prev_iter_inside_quote, &qb2, &error_mask)
+		}
+	}
+}
