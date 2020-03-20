7e26c804fd23b5a77a2a3f18ea6327f7867c2b87
frankw <fwessels@xs4all.nl>
2020-03-20 16:37:00 -0700

    Update find marks to detect quotes

--- find_marks_amd64.go
+++ find_marks_amd64.go
@@ -1,4 +1,5 @@
 package simdcsv
 
 //go:noescape
-func find_marks_in_slice(msg []byte, indexes *[INDEXES_SIZE]uint32, indexes_length *uint64) (out uint64)
+func find_marks_in_slice(msg []byte, indexes *[INDEXES_SIZE]uint32, indexes_length *uint64,
+					     odd_ends, prev_iter_inside_quote, quote_bits, error_mask *uint64) (out uint64)
--- find_marks_amd64.s
+++ find_marks_amd64.s
@@ -12,8 +12,14 @@ TEXT ·find_marks_in_slice(SB), 7, $0
     VMOVDQU    0x20(DI), Y9      // load high 32-bytes
 
     CALL ·__find_separator(SB)
+    PUSHQ       AX
 
-	MOVQ        AX, out+40(FP)
+    MOVQ odd_ends+40(FP), DX
+    MOVQ prev_iter_inside_quote+48(FP), CX
+    MOVQ quote_bits+56(FP), R8
+    MOVQ error_mask+64(FP), R9
+
+    CALL ·__find_quote_mask_and_bits(SB)
 
     MOVQ        indexes+24(FP), DI
     MOVQ        indexes_length+32(FP), SI
@@ -23,6 +29,11 @@ TEXT ·find_marks_in_slice(SB), 7, $0
     MOVQ        (SI), BX // , INDEX
     MOVQ        $0, DX   // , CARRIED
     MOVQ        $0, R10  // , POSITION
+
+    MOVQ        quote_bits+56(FP), R15
+    MOVQ        (R15), R15
+
+    POPQ        AX
     CALL ·__flatten_bits_incremental(SB)
     // MOVQ     POSITION, (R12)
     // MOVQ     CARRIED, (R11)
