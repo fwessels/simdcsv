d40563f33bcb46ea177b6e72b333225a6609d263
frankw <fwessels@xs4all.nl>
2020-03-23 14:06:36 -0700

    Test case for ignoring separators enclosed in quotes

--- stage1_find_marks_amd64_test.go
+++ stage1_find_marks_amd64_test.go
@@ -15,6 +15,7 @@ func TestStage1FindMarks(t *testing.T) {
 	vectors := []string{
 		`1103341116,2015-12-21T00:00:00,1251,,,CA,200304,,HOND,PA,GY,13147 WELBY WAY,01521,1,4000A1,NO EVIDENCE OF REG,50,99999,99999    `,
 		`1103341116,2015-12-21T00:00:00,1251,,,CA,200304,,HOND,PA,GY,13147 WELBY WAY,01521,1,4000A1,"NO EVIDENCE OF REG",50,99999,99999 `,
+		`1103341116,2015-12-21T00:00:00,1251,,,CA,200304,,HOND,PA,GY,13147 WELBY WAY,01521,1,4000A1,"NO EVIDENCE,OF REG",50,99999,99999 `,
 	}
 
 	want := []string{"7 WELBY WAY", "01521", "1", "4000A1", "NO EVIDENCE OF REG", "50", ""}
@@ -24,6 +25,13 @@ func TestStage1FindMarks(t *testing.T) {
 		t.Errorf("TestStage1FindMarks: got: %v want: %v", record, want)
 	}
 	record = Stage1FindMarks([]byte(vectors[1][64:]))
+	if !reflect.DeepEqual(record, want) {
+		t.Errorf("TestStage1FindMarks: got: %v want: %v", record, want)
+	}
+
+	// Reflect (ignored) comma in expected results
+	want[4] = "NO EVIDENCE,OF REG"
+	record = Stage1FindMarks([]byte(vectors[2][64:]))
 	if !reflect.DeepEqual(record, want) {
 		t.Errorf("TestStage1FindMarks: got: %v want: %v", record, want)
 	}
