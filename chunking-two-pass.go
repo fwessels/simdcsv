d06d2d33ad4830bd06132dc2319ed8487fe1e8f9
frankw <fwessels@xs4all.nl>
2020-07-07 17:00:12 -0700

    Handle corner case when last char is a quote

--- chunking-two-pass.go
+++ chunking-two-pass.go
@@ -4,6 +4,7 @@ type chunkInfo struct {
 	quotes                int
 	positionDelimiterEven int
 	positionDelimiterOdd  int
+	firstCharIsQuote      bool
 	lastCharIsQuote       bool
 }
 
@@ -32,7 +33,42 @@ func ChunkTwoPass(buf []byte) (ci chunkInfo) {
 		}
 	}
 
+	ci.firstCharIsQuote = buf[0] == '"'
 	ci.lastCharIsQuote = buf[len(buf)-1] == '"'
+
+	return
+}
+
+func GetWidowSizes(chunkInfos []chunkInfo) (widowSizes []int) {
+
+	widowSizes = make([]int, 0, 100)
+
+	totalQuotes := 0
+	prevChunkLastCharIsQuote := false
+
+	for i, ci := range chunkInfos {
+		afterFirstDelim := 0
+		quoteCorrection := 0
+		if i != 0 {
+			even, odd := ci.positionDelimiterEven+1, ci.positionDelimiterOdd+1
+			if prevChunkLastCharIsQuote && ci.firstCharIsQuote {
+				// we have an escaped quote precisely at the split between chunks
+				quoteCorrection = 1
+				// and swap even and odd (detected exactly oppositve)
+				even, odd = odd, even
+			}
+
+			if totalQuotes&1 == 0 {
+				afterFirstDelim = even
+			} else {
+				afterFirstDelim = odd
+			}
+		}
+		widowSizes = append(widowSizes, afterFirstDelim)
+
+		totalQuotes += ci.quotes - quoteCorrection
+		prevChunkLastCharIsQuote = ci.lastCharIsQuote
+	}
 
 	return
 }
--- chunking-two-pass_test.go
+++ chunking-two-pass_test.go
@@ -1,8 +1,10 @@
 package simdcsv
 
 import (
-	_ "fmt"
+	"encoding/hex"
+	"fmt"
 	"io/ioutil"
+	"reflect"
 	"testing"
 )
 
@@ -104,23 +106,51 @@ func testTwoPassChain(t *testing.T, filename string, chunkSize int) {
 		return
 	}
 
-	totalQuotes := 0
-	for i, ci := range chunkInfos {
-		afterFirstDelim := ci.positionDelimiterEven + 1
-		if i == 0 {
-			afterFirstDelim = 0
-		} else if totalQuotes&1 == 1 {
-			afterFirstDelim = ci.positionDelimiterOdd + 1
-		}
+	widowSizes := GetWidowSizes(chunkInfos)
 
-		if uint64(afterFirstDelim) != sourceOfTruth[i].widowSize {
-			t.Errorf("TestChunkTwoPass[%d]: got: %v want: %v", i, afterFirstDelim, sourceOfTruth[i].widowSize)
+	for i, widowSize := range widowSizes {
+		if widowSize != int(sourceOfTruth[i].widowSize) {
+			t.Errorf("TestChunkTwoPass[%d]: got: %v want: %v", i, widowSize, sourceOfTruth[i].widowSize)
 		}
-
-		totalQuotes += ci.quotes
 	}
 }
 
 func TestTwoPassChain(t *testing.T) {
 	testTwoPassChain(t, "test-data/Emails.csv", 256*1024)
+}
+
+func TestLastCharIsQuote(t *testing.T) {
+
+	const file = `first_name,last_name,username
+"Robert","Pike",rob
+Kenny,Thompson,kenny
+"Robert","Griesemer","gr""i"
+Donald,Du"
+"ck,don
+Dagobert,Duck,dago
+`
+	fmt.Println(hex.Dump([]byte(file)))
+
+	// 00000000  66 69 72 73 74 5f 6e 61  6d 65 2c 6c 61 73 74 5f  |first_name,last_|
+	// 00000010  6e 61 6d 65 2c 75 73 65  72 6e 61 6d 65 0a 22 52  |name,username."R|
+	// 00000020  6f 62 65 72 74 22 2c 22  50 69 6b 65 22 2c 72 6f  |obert","Pike",ro|
+	// 00000030  62 0a 4b 65 6e 6e 79 2c  54 68 6f 6d 70 73 6f 6e  |b.Kenny,Thompson|
+	// 00000040  2c 6b 65 6e 6e 79 0a 22  52 6f 62 65 72 74 22 2c  |,kenny."Robert",|
+	// 00000050  22 47 72 69 65 73 65 6d  65 72 22 2c 22 67 72 22  |"Griesemer","gr"|
+	// 00000060  22 69 22 0a 44 6f 6e 61  6c 64 2c 44 75 22 0a 22  |"i".Donald,Du"."|
+	// 00000070  63 6b 2c 64 6f 6e 0a 44  61 67 6f 62 65 72 74 2c  |ck,don.Dagobert,|
+	// 00000080  44 75 63 6b 2c 64 61 67  6f 0a                    |Duck,dago.|
+
+	chunkInfos := make([]chunkInfo, 0, 100)
+
+	chunkInfos = append(chunkInfos, ChunkTwoPass([]byte(file)[0:0x60]))
+	chunkInfos = append(chunkInfos, ChunkTwoPass([]byte(file)[0x60:]))
+
+	widowSizes := GetWidowSizes(chunkInfos)
+
+	expected := []int{0, 4}
+
+	if !reflect.DeepEqual(widowSizes, expected) {
+		t.Errorf("TestLastCharIsQuote: got: %v want: %v", widowSizes, expected)
+	}
 }
