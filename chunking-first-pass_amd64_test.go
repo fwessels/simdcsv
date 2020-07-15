e3f29af293a1b0a785979fbec9a3517d889674c9
frankw <fwessels@xs4all.nl>
2020-07-14 17:02:14 -0700

    Re-enable testing code for assembly

--- chunking-first-pass_amd64_test.go
+++ chunking-first-pass_amd64_test.go
@@ -203,18 +203,18 @@ func TestHandleMasks(t *testing.T) {
 		quotes, even, odd := uint64(0), -1, -1
 		handleMasks(tc.quoteMask, tc.newlineMask, tc.nextCharIsQuote, &quotes, &even, &odd)
 
-		//quotesAsm, evenAsm, oddAsm := uint64(0), -1, -1
-		//handle_masks(tc.quoteMask, tc.newlineMask, &quotesAsm, &evenAsm, &oddAsm)
-		//
-		//if quotes != quotesAsm {
-		//	t.Errorf("TestHandleMasks(%d): mismatch for asm: %d want: %d", i, quotes, quotesAsm)
-		//}
-		//if even != evenAsm {
-		//	t.Errorf("TestHandleMasks(%d): mismatch for asm: %d want: %d", i, even, evenAsm)
-		//}
-		//if odd != oddAsm {
-		//	t.Errorf("TestHandleMasks(%d): mismatch for asm: %d want: %d", i, odd, oddAsm)
-		//}
+		quotesAsm, evenAsm, oddAsm := uint64(0), -1, -1
+		handle_masks(tc.quoteMask, tc.newlineMask, tc.nextCharIsQuote, &quotesAsm, &evenAsm, &oddAsm)
+
+		if quotes != quotesAsm {
+			t.Errorf("TestHandleMasks(%d): mismatch for asm: %d want: %d", i, quotes, quotesAsm)
+		}
+		if even != evenAsm {
+			t.Errorf("TestHandleMasks(%d): mismatch for asm: %d want: %d", i, even, evenAsm)
+		}
+		if odd != oddAsm {
+			t.Errorf("TestHandleMasks(%d): mismatch for asm: %d want: %d", i, odd, oddAsm)
+		}
 
 		if quotes != tc.expectedQuotes {
 			t.Errorf("TestHandleMasks(%d): got: %d want: %d", i, quotes, tc.expectedQuotes)
