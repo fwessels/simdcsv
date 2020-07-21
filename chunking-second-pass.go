89aa7a7d7cc40b4751964daa5b7197cbeeec50f8
frankw <fwessels@xs4all.nl>
2020-07-21 14:51:41 -0700

    Framework for parsing for second pass

--- chunking-second-pass.go
+++ chunking-second-pass.go
@@ -1,5 +1,9 @@
 package simdcsv
 
+import (
+	"fmt"
+	"math/bits"
+)
 const PreprocessedDelimiter = 0x0
 const PreprocessedSeparator = 0x1
 
@@ -47,4 +51,66 @@ func PreprocessDoubleQuotes(in []byte) (out []byte) {
 	}
 
 	return
+}
+
+func SecondPass(buffer []byte) {
+
+	containsDoubleQuotes := true
+
+	delimiter, separator, quote := '\n', ',', '"'
+	buf := buffer
+	if containsDoubleQuotes {
+		buf = PreprocessDoubleQuotes(buffer)
+
+		delimiter, separator, quote = PreprocessedDelimiter, PreprocessedSeparator, 0x02
+	}
+
+	ParseSecondPass(buf, delimiter, separator, quote)
+}
+
+func ParseSecondPass(buffer []byte, delimiter, separator, quote rune) {
+
+	separatorMasks := getBitMasks([]byte(buffer), byte(separator))
+	delimiterMasks := getBitMasks([]byte(buffer), byte(delimiter))
+	quoteMasks := getBitMasks([]byte(buffer), byte(quote))
+
+	fmt.Printf(" separator: %064b\n", separatorMasks[0])
+	fmt.Printf(" delimiter: %064b\n", delimiterMasks[0])
+	fmt.Printf("     quote: %064b\n", quoteMasks[0])
+
+	ParseSecondPassMasks(separatorMasks[0], delimiterMasks[0], quoteMasks[0])
+}
+
+func ParseSecondPassMasks(separatorMask, delimiterMask, quoteMask uint64) {
+
+	const clearMask = 0xfffffffffffffffe
+
+	separatorPos := bits.TrailingZeros64(separatorMask)
+	delimiterPos := bits.TrailingZeros64(delimiterMask)
+	quotePos := bits.TrailingZeros64(quoteMask)
+
+	for {
+		if separatorPos < delimiterPos && separatorPos < quotePos {
+
+			fmt.Println("Encountered separator at", separatorPos)
+			separatorMask &= clearMask << separatorPos
+			separatorPos = bits.TrailingZeros64(separatorMask)
+
+		} else if delimiterPos < separatorPos && delimiterPos < quotePos {
+
+			fmt.Println("Encountered delimiter at", delimiterPos)
+			delimiterMask &= clearMask << delimiterPos
+			delimiterPos = bits.TrailingZeros64(delimiterMask)
+
+		} else if quotePos < separatorPos && quotePos < delimiterPos {
+
+			fmt.Println("Encountered qoute at", quotePos)
+			quoteMask &= clearMask << quotePos
+			quotePos = bits.TrailingZeros64(quoteMask)
+
+		} else {
+			// we must be done
+			break
+		}
+	}
 }
