3f5a6c0107f5bec4ef9d7b3f018a47211c1b5e3a
frankw <fwessels@xs4all.nl>
2020-08-10 14:37:19 -0700

    Integrate previous testcase for parking citations

--- simdcsv.go
+++ simdcsv.go
@@ -1,1 +1,19 @@
 package simdcsv
+
+import (
+	"unsafe"
+)
+
+func ReadAll(buf []byte) (rows [][]string) {
+
+	input := Input{base: uint64(uintptr(unsafe.Pointer(&buf[0])))}
+	rows = make([][]string, 10000 + 100)
+	columns := make([]string, len(rows)*100)
+	output := OutputAsm{unsafe.Pointer(&columns[0]), 1, unsafe.Pointer(&rows[0]), 0, uint64(uintptr(unsafe.Pointer(&columns[0]))), 0, uint64(cap(columns))}
+
+	parse_block_second_pass(buf, '\n', ',', '"', &input, 0, &output)
+
+	rows = rows[:output.line/3]
+
+	return
+}
