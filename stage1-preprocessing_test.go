b1bd49949edc065fe92ebf2dd8b690771f50a481
frankw <fwessels@xs4all.nl>
2020-08-28 09:30:21 -0700

    Test case reformatting for processing

--- stage1-preprocessing_test.go
+++ stage1-preprocessing_test.go
@@ -27,53 +27,32 @@ func TestStage1Preprocessing(t *testing.T) {
 	Stage2Parse(buf, delimiter, separator, quote, stage2_parse_test)
 }
 
+func testStage1PreprocessDoubleQuotes(t *testing.T, data []byte) {
+
+	preprocessed := preprocessDoubleQuotes(data)
+
+	simdrecords := Stage2ParseBuffer(preprocessed, preprocessedDelimiter, preprocessedSeparator, preprocessedQuote, nil)
+	records := EncodingCsv(data)
+
+	if !reflect.DeepEqual(simdrecords, records) {
+		t.Errorf("testStage1PreprocessDoubleQuotes: got: %v want: %v", simdrecords, records)
+	}
+}
+
 func TestStage1PreprocessDoubleQuotes(t *testing.T) {
 
-	const file = `first_name,last_name,username
+	t.Run("simple", func(t *testing.T) {
+
+		const data = `first_name,last_name,username
 "Robert","Pike",rob
 Kenny,Thompson,kenny
 "Robert","Griesemer","gr""i"
-Donald,Du""c
-k,don
+Donald,"Du""c
+k",don
 Dagobert,Duck,dago
 `
+		testStage1PreprocessDoubleQuotes(t, []byte(data))
+	})
 
-	// 00000000  66 69 72 73 74 5f 6e 61  6d 65 2c 6c 61 73 74 5f  |first_name,last_|
-	// 00000010  6e 61 6d 65 2c 75 73 65  72 6e 61 6d 65 0a 22 52  |name,username."R|
-	// 00000020  6f 62 65 72 74 22 2c 22  50 69 6b 65 22 2c 72 6f  |obert","Pike",ro|
-	// 00000030  62 0a 4b 65 6e 6e 79 2c  54 68 6f 6d 70 73 6f 6e  |b.Kenny,Thompson|
-	// 00000040  2c 6b 65 6e 6e 79 0a 22  52 6f 62 65 72 74 22 2c  |,kenny."Robert",|
-	// 00000050  22 47 72 69 65 73 65 6d  65 72 22 2c 22 67 72 22  |"Griesemer","gr"|
-	// 00000060  22 69 22 0a 44 6f 6e 61  6c 64 2c 44 75 22 22 63  |"i".Donald,Du""c|
-	// 00000070  0a 6b 2c 64 6f 6e 0a 44  61 67 6f 62 65 72 74 2c  |.k,don.Dagobert,|
-	// 00000080  44 75 63 6b 2c 64 61 67  6f 0a                    |Duck,dago.|
-
-	preprocessed := preprocessDoubleQuotes([]byte(file))
-
-	fmt.Println(hex.Dump(preprocessed))
-
-	// 00000000  66 69 72 73 74 5f 6e 61  6d 65 02 6c 61 73 74 5f  |first_name.last_|
-	// 00000010  6e 61 6d 65 02 75 73 65  72 6e 61 6d 65 01 52 6f  |name.username.Ro|
-	// 00000020  62 65 72 74 02 50 69 6b  65 02 72 6f 62 01 4b 65  |bert.Pike.rob.Ke|
-	// 00000030  6e 6e 79 02 54 68 6f 6d  70 73 6f 6e 02 6b 65 6e  |nny.Thompson.ken|
-	// 00000040  6e 79 01 52 6f 62 65 72  74 02 47 72 69 65 73 65  |ny.Robert.Griese|
-	// 00000050  6d 65 72 02 67 72 22 69  01 44 6f 6e 61 6c 64 02  |mer.gr"i.Donald.|
-	// 00000060  44 75 63 01 6b 02 64 6f  6e 01 44 61 67 6f 62 65  |Duc.k.don.Dagobe|
-	// 00000070  72 74 02 44 75 63 6b 02  64 61 67 6f 01           |rt.Duck.dago.|
-
-	lines := bytes.Split([]byte(preprocessed), []byte{preprocessedDelimiter})
-	for _, line := range lines {
-		fields := bytes.Split([]byte(line), []byte{preprocessedSeparator})
-		for i, field := range fields {
-			fmt.Print(string(field))
-			if i < len(fields)-1 {
-				fmt.Print(",")
-			}
-		}
-		fmt.Println()
-	}
-
-	records := Stage2ParseBuffer(preprocessed, preprocessedDelimiter, preprocessedSeparator, preprocessedQuote, nil)
-	fmt.Println(records)
 }
 
