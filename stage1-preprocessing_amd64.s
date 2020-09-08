787095779776e14d2af177cae45a2dd20ad08ea6
frankw <fwessels@xs4all.nl>
2020-09-08 16:10:01 -0700

    More optimised assembly (inverted)

--- stage1-preprocessing_amd64.s
+++ stage1-preprocessing_amd64.s
@@ -176,6 +176,9 @@ TEXT ·stage1_preprocess(SB), 7, $0
 	BSFQ    SI, DI
 	MOVQ    (AX), R8
 	BSFQ    R8, R9
+	MOVQ    DX, 0x8(R10)
+	MOVQ    0x10(AX), R11
+	MOVQ    R11, 0x10(R10)
 	BSFQ    DX, DX
 	MOVL    $0x40, DX
 	CMOVQEQ DX, BX
@@ -183,111 +186,109 @@ TEXT ·stage1_preprocess(SB), 7, $0
 	CMOVQEQ DX, DI
 	BSFQ    R8, SI
 	CMOVQEQ DX, R9
-	MOVQ    R10, SI
 	JMP     label2
 
 label1:
-	MOVQ R8, R9
+	MOVQ SI, R9
 
 label2:
 	CMPQ R9, BX
 	JGE  label8
 	CMPQ R9, DI
 	JGE  label8
-	MOVQ 0x20(AX), R8
-	CMPQ R8, $0x0
+	MOVQ 0x20(AX), SI
+	CMPQ SI, $0x0
 	JE   label5
 	CMPQ R9, $0x3f
 	JNE  label7
-	MOVQ 0x18(AX), R10
-	ANDQ $0x1, R10
-	CMPQ R10, $0x1
+	MOVQ 0x18(AX), R8
+	ANDQ $0x1, R8
+	CMPQ R8, $0x1
 	JNE  label4
 	MOVQ R9, CX
-	MOVQ $-0x2, R8
-	SHLQ CL, R8
-	ANDQ R8, (AX)
+	MOVQ $-0x2, SI
+	SHLQ CL, SI
+	ANDQ SI, (AX)
 	ANDQ $-0x2, 0x18(AX)
 
 label3:
-	MOVQ    (AX), R8
-	BSFQ    R8, R8
-	CMOVQEQ DX, R8
+	MOVQ    (AX), SI
+	BSFQ    SI, SI
+	CMOVQEQ DX, SI
 	JMP     label1
 
 label4:
-	CMPQ R8, $0x0
+	CMPQ SI, $0x0
 
 label5:
 	JE    label6
-	MOVQ  (AX), R8
+	MOVQ  (AX), SI
 	LEAQ  0x1(R9), CX
 	CMPQ  CX, $0x40
-	SBBQ  R10, R10
+	SBBQ  R8, R8
 	MOVL  $0x1, R11
 	SHLQ  CL, R11
-	ANDQ  R10, R11
-	TESTQ R8, R11
+	ANDQ  R8, R11
+	TESTQ SI, R11
 	JE    label6
 	MOVQ  $-0x2, R9
 	SHLQ  CL, R9
-	ANDQ  R10, R9
 	ANDQ  R8, R9
+	ANDQ  SI, R9
 	MOVQ  R9, (AX)
 	JMP   label3
 
 label6:
-	TESTB AL, (SI)
-	CMPQ  R9, $0x40
-	SBBQ  R8, R8
-	MOVQ  R9, CX
-	MOVL  $0x1, R10
-	SHLQ  CL, R10
-	ANDQ  R8, R10
-	ORQ   R10, (SI)
-	MOVQ  0x20(AX), R10
-	NOTQ  R10
-	MOVQ  R10, 0x20(AX)
-	MOVQ  $-0x2, R10
-	SHLQ  CL, R10
-	ANDQ  R10, R8
-	ANDQ  R8, (AX)
-	JMP   label3
+	CMPQ R9, $0x40
+	SBBQ SI, SI
+	MOVQ R9, CX
+	MOVL $0x1, R8
+	SHLQ CL, R8
+	ANDQ SI, R8
+	ORQ  R8, (R10)
+	MOVQ 0x20(AX), R8
+	NOTQ R8
+	MOVQ R8, 0x20(AX)
+	MOVQ $-0x2, R8
+	SHLQ CL, R8
+	ANDQ R8, SI
+	ANDQ SI, (AX)
+	JMP  label3
 
 label7:
-	CMPQ R8, $0x0
+	CMPQ SI, $0x0
 	JMP  label5
 
 label8:
-	CMPQ  BX, R9
-	JGE   label12
-	CMPQ  BX, DI
-	JGE   label12
-	CMPQ  0x20(AX), $0x0
-	JNE   label11
-	TESTB AL, (SI)
-	CMPQ  BX, $0x40
-	SBBQ  R8, R8
-	MOVQ  BX, CX
-	MOVL  $0x1, R10
-	SHLQ  CL, R10
-	ANDQ  R8, R10
-	ORQ   R10, 0x8(SI)
+	CMPQ BX, R9
+	JGE  label12
+	CMPQ BX, DI
+	JGE  label12
+	CMPQ 0x20(AX), $0x0
+	JE   label11
+	CMPQ BX, $0x40
+	SBBQ SI, SI
+	MOVQ BX, CX
+	MOVL $0x1, R8
+	SHLQ CL, R8
+	ANDQ SI, R8
+	NOTQ R8
+	ANDQ R8, 0x8(R10)
 
 label9:
-	MOVQ    0x8(AX), R8
+	MOVQ    0x8(AX), SI
 	CMPQ    CX, $0x40
-	SBBQ    R10, R10
+	SBBQ    R8, R8
 	MOVQ    $-0x2, R11
 	SHLQ    CL, R11
-	ANDQ    R10, R11
 	ANDQ    R8, R11
+	ANDQ    SI, R11
 	BSFQ    R11, BX
 	MOVQ    R11, 0x8(AX)
 	CMOVQEQ DX, BX
 
 label10:
-	MOVQ R9, R8
+	MOVQ R9, SI
 	JMP  label1
 
 label11:
