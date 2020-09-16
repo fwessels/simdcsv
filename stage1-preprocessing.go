c280870bceb19383ebda46fa6b605f8df3a6e6b5
frankw <fwessels@xs4all.nl>
2020-09-16 08:48:59 -0700

    Remove old code for replacing Double Quotes in place

--- stage1-preprocessing.go
+++ stage1-preprocessing.go
@@ -18,55 +18,6 @@ const preprocessedDelimiter = 0x1
 const preprocessedSeparator = 0x2
 const preprocessedQuote = 0x3
 
-func preprocessDoubleQuotes(in []byte) (out []byte) {
-
-	// Replace delimiter and separators
-	// Remove any surrounding quotes
-	// Replace double quotes with single quote
-
-	out = make([]byte, 0, len(in))
-	quoted := false
-
-	for i := 0; i < len(in); i++ {
-		b := in[i]
-
-		if quoted {
-			if b == '"' && i+1 < len(in) && in[i+1] == '"' {
-				// replace escaped quote with single quote
-				out = append(out, '"')
-				// and skip next char
-				i += 1
-			} else if b == '"' {
-				quoted = false
-			} else if b == '\r' && i+1 < len(in) && in[i+1] == '\n' {
-				out = append(out, '\n') // replace \r\n in quoted fields with single \n (conform to encoding/csv behavior)
-				i += 1
-			} else {
-				out = append(out, b)
-			}
-		} else {
-			if b == '"' {
-				quoted = true
-			} else if b == '\r' && i+1 < len(in) && in[i+1] == '\n' {
-				// replace delimiter with '\1'
-				out = append(out, preprocessedDelimiter)
-				// and skip next char
-				i += 1
-			} else if b == '\n' {
-				// replace delimiter with '\1'
-				out = append(out, preprocessedDelimiter)
-			} else if b == ',' {
-				// replace separator with '\2'
-				out = append(out, preprocessedSeparator)
-			} else {
-				out = append(out, b)
-			}
-		}
-	}
-
-	return
-}
-
 type stage1Input struct {
 	quoteMaskIn          uint64
 	separatorMaskIn      uint64
--- stage1-preprocessing_test.go
+++ stage1-preprocessing_test.go
@@ -12,48 +12,6 @@ import (
 	"testing"
 )
 
-func testStage1PreprocessDoubleQuotes(t *testing.T, data []byte) {
-
-	preprocessed := preprocessDoubleQuotes(data)
-
-	simdrecords := Stage2ParseBuffer(preprocessed, preprocessedDelimiter, preprocessedSeparator, preprocessedQuote, nil)
-	records := EncodingCsv(data)
-
-	if !reflect.DeepEqual(simdrecords, records) {
-		t.Errorf("testStage1PreprocessDoubleQuotes: got: %v want: %v", simdrecords, records)
-	}
-}
-
-func TestStage1PreprocessDoubleQuotes(t *testing.T) {
-
-	const first = `first_name,last_name,username
-"Robert","Pike",rob
-Kenny,Thompson,kenny
-"Robert","Griesemer","gr""i"
-Donald,"Du""c`
-
-	const second = `k",don
-Dagobert,Duck,dago
-`
-	t.Run("double-quotes", func(t *testing.T) {
-
-		const data = first + second
-		testStage1PreprocessDoubleQuotes(t, []byte(data))
-	})
-
-	t.Run("newline-in-quoted-field", func(t *testing.T) {
-
-		const data = first + "\n" + second
-		testStage1PreprocessDoubleQuotes(t, []byte(data))
-	})
-
-	t.Run("carriage-return-in-quoted-field", func(t *testing.T) {
-
-		const data = first + "\r\n" + second
-		testStage1PreprocessDoubleQuotes(t, []byte(data))
-	})
-}
-
 func TestStage1PreprocessMasksToMasks(t *testing.T) {
 
 	t.Run("go", func(t *testing.T) {
