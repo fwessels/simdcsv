43f81199ede923c9d5a0c31c1889de351a9d5c4e
frankw <fwessels@xs4all.nl>
2020-09-25 17:24:27 -0700

    Fully integrated version for first and second stages

--- stages-combined_amd64.s
+++ stages-combined_amd64.s
@@ -37,7 +37,7 @@
 #define Y_NEWLINE     Y2
 #define Y_DELIMITER   Y_NEWLINE
 
-// func stages_combined_buffer(buf []byte, separatorChar uint64, input *stage1Input, output *stage1Output)
+// func stages_combined_buffer(buf []byte, separatorChar uint64, input1 *stage1Input, output1 *stage1Output)
 TEXT ·stages_combined_buffer(SB), 7, $0
 
 	MOVQ         $0x0a, AX                // character for newline
@@ -61,27 +61,26 @@ TEXT ·stages_combined_buffer(SB), 7, $0
 
 	MOVQ output2+72(FP), BX
 	MOVQ rows_base+88(FP), AX
-	MOVQ AX, ROWS_BASE(BX)       // initialize rows base pointer
+	MOVQ AX, ROWS_BASE(BX)        // initialize rows base pointer
 	MOVQ columns_base+112(FP), AX
-	MOVQ AX, COLUMNS_BASE(BX)    // initialize columns base pointer
-
+	MOVQ AX, COLUMNS_BASE(BX)     // initialize columns base pointer
 
 	MOVQ offset+56(FP), DX
 
-    MOVQ DX, CX
-    ADDQ $0x40, CX
-    CMPQ CX, buf_len+8(FP)
-    JLE  fullLoadPrologue
-    MOVQ buf_len+8(FP), BX
-    CALL ·partialLoad(SB)
-    JMP  skipFullLoadPrologue
+	MOVQ DX, CX
+	ADDQ $0x40, CX
+	CMPQ CX, buf_len+8(FP)
+	JLE  fullLoadPrologue
+	MOVQ buf_len+8(FP), BX
+	CALL ·partialLoad(SB)
+	JMP  skipFullLoadPrologue
 
 fullLoadPrologue:
 	VMOVDQU (DI)(DX*1), Y6     // load low 32-bytes
 	VMOVDQU 0x20(DI)(DX*1), Y7 // load high 32-bytes
 
 skipFullLoadPrologue:
-	MOVQ input+32(FP), SI
+	MOVQ input1+32(FP), SI
 
 	// quote mask
 	VPCMPEQB Y6, Y_QUOTE_CHAR, Y0
@@ -103,10 +102,23 @@ skipAddTrailingNewlinePrologue:
 	MOVQ BX, NEWLINE_MASK_IN_NEXT(SI) // store in next slot, so that it gets copied back
 
 loop:
+	//  Check whether there is still enough reserved space in the rows and columns destination buffer
+	MOVQ output2+72(FP), BX
+	MOVQ INDEX_OFFSET(BX), AX    // load output.index
+	SHRQ $1, AX                  // divide by 2 to get number of strings (since we write two words per string)
+	ADDQ $64, AX                 // absolute maximum of strings to be potentially written per 64 bytes
+	CMPQ AX, columns_len+120(FP)
+	JGE  done                    // exit out and make sure more memory is allocated
+
+	MOVQ LINE_OFFSET(BX), AX // load output.line
+	ADDQ $64, AX             // absolute maximum of lines to be potentially written per 64 bytes
+	CMPQ AX, rows_len+96(FP)
+	JGE  done                // exit out and make sure more memory is allocated
+
 	VMOVDQU Y6, Y8 // get low 32-bytes
 	VMOVDQU Y7, Y9 // get high 32-bytes
 
-	MOVQ input+32(FP), SI
+	MOVQ input1+32(FP), SI
 
 	// copy next masks to current slot (for quote mask and newline mask)
 	MOVQ QUOTE_MASK_IN_NEXT(SI), CX
@@ -126,19 +138,19 @@ loop:
 	CREATE_MASK(Y0, Y1, AX, CX)
 	MOVQ     CX, CARRIAGE_RETURN_MASK_IN(SI)
 
-    // do we need to do a partial load?
-    MOVQ DX, CX
-    ADDQ $0x80, CX
-    CMPQ CX, buf_len+8(FP)
-    JLE  fullLoad
-    MOVQ buf_len+8(FP), BX
-    CALL ·partialLoad(SB)
-    JMP  skipFullLoad
+	// do we need to do a partial load?
+	MOVQ DX, CX
+	ADDQ $0x80, CX
+	CMPQ CX, buf_len+8(FP)
+	JLE  fullLoad
+	MOVQ buf_len+8(FP), BX
+	CALL ·partialLoad(SB)
+	JMP  skipFullLoad
 
 fullLoad:
-    // load next pair of YMM words
-    VMOVDQU 0x40(DI)(DX*1), Y6 // load low 32-bytes of next pair
-    VMOVDQU 0x60(DI)(DX*1), Y7 // load high 32-bytes of next pair
+	// load next pair of YMM words
+	VMOVDQU 0x40(DI)(DX*1), Y6 // load low 32-bytes of next pair
+	VMOVDQU 0x60(DI)(DX*1), Y7 // load high 32-bytes of next pair
 
 skipFullLoad:
 	VPCMPEQB Y6, Y_QUOTE_CHAR, Y0
@@ -160,12 +172,12 @@ skipAddTrailingNewline:
 	MOVQ BX, NEWLINE_MASK_IN_NEXT(SI)
 
 	PUSHQ DX
-	MOVQ  input+32(FP), AX
-	MOVQ  output+40(FP), R10
+	MOVQ  input1+32(FP), AX
+	MOVQ  output1+40(FP), R10
 	CALL  ·stage1_preprocess(SB)
 	POPQ  DX
 
-	MOVQ output+40(FP), R10
+	MOVQ output1+40(FP), R10
 
 	MOVQ input2+64(FP), SI
 
@@ -173,36 +185,37 @@ skipAddTrailingNewline:
 	VPCMPEQB Y8, Y_DELIMITER, Y0
 	VPCMPEQB Y9, Y_DELIMITER, Y1
 	CREATE_MASK(Y0, Y1, AX, BX)
+
 	// Merge in carridage returns that are treated as newlines (and hence filtered out)
 	ORQ CARRIAGE_RETURN_MASK_OUT(R10), BX
 
-    // are we processing the last 64-bytes?
-    MOVQ DX, AX
-    ADDQ $0x40, AX
-    CMPQ AX, buf_len+8(FP)
-    JLE  notLastZWord
+	// are we processing the last 64-bytes?
+	MOVQ DX, AX
+	ADDQ $0x40, AX
+	CMPQ AX, buf_len+8(FP)
+	JLE  notLastZWord
 
-    // Check if we need to OR in closing delimiter into last delimiter mask
-    // We only do this the buffer is not already terminated with a delimiter
-    MOVQ lastCharIsDelimiter+80(FP), CX
-    CMPQ CX, $1
-    JZ   notLastZWord
-    MOVQ buf_len+8(FP), CX
-    ANDQ $0x3f, CX
-    MOVQ $1, AX
-    SHLQ CX, AX
-    ORQ  AX, BX
+	// Check if we need to OR in closing delimiter into last delimiter mask
+	// We only do this the buffer is not already terminated with a delimiter
+	MOVQ lastCharIsDelimiter+80(FP), CX
+	CMPQ CX, $1
+	JZ   notLastZWord
+	MOVQ buf_len+8(FP), CX
+	ANDQ $0x3f, CX
+	MOVQ $1, AX
+	SHLQ CX, AX
+	ORQ  AX, BX
 
 notLastZWord:
-    MOVQ BX, 8(SI)
+	MOVQ BX, 8(SI)
 
 	// separator mask
-	MOVQ     SEPARATOR_MASK_OUT(R10), CX
-	MOVQ     CX, 0(SI)
+	MOVQ SEPARATOR_MASK_OUT(R10), CX
+	MOVQ CX, 0(SI)
 
 	// quote mask
-	MOVQ     QUOTE_MASK_OUT(R10), CX
-	MOVQ     CX, 16(SI)
+	MOVQ QUOTE_MASK_OUT(R10), CX
+	MOVQ CX, 16(SI)
 
 	MOVQ offset+56(FP), DI
 	MOVQ output2+72(FP), R9
@@ -212,15 +225,7 @@ notLastZWord:
 	CALL  ·stage2_parse(SB)
 	POPQ  DX
 
-	ADDQ $0x40, offset+56(FP)
-	ADDQ $0x40, DX
-	CMPQ DX, buf_len+8(FP)
-	JLT  loop
-//	JNZ  done                  // in case we end exactly on a 64-byte boundary, check if we need to add a delimiter
-RET
-
-
-	MOVQ output+40(FP), R10
+	MOVQ output1+40(FP), R10
 	CMPQ NEEDS_POST_PROCESSING_OUT(R10), $1
 	JNZ  unmodified
 
@@ -236,68 +241,36 @@ RET
 	SUBQ $0x40, DX
 
 unmodified:
+	ADDQ $0x40, offset+56(FP)
 	ADDQ $0x40, DX
 	CMPQ DX, buf_len+8(FP)
 	JLT  loop
-
-exit:
-	MOVQ DX, processed+136(FP)
-	RET
-
-
-
-
-
-// func _stage2_parse_buffer()
-TEXT ·_stage2_PRUNE(SB), 7, $0
-
-
-	MOVQ offset+112(FP), DX
-
-loop:
-	//  Check whether there is still enough reserved space in the rows and columns destination buffer
-	MOVQ output+120(FP), BX
-	MOVQ INDEX_OFFSET(BX), AX   // load output.index
-	SHRQ $1, AX                 // divide by 2 to get number of strings (since we write two words per string)
-	ADDQ $64, AX                // absolute maximum of strings to be potentially written per 64 bytes
-	CMPQ AX, columns_len+64(FP)
-	JGE  done                   // exit out and make sure more memory is allocated
-
-	MOVQ LINE_OFFSET(BX), AX // load output.line
-	ADDQ $64, AX             // absolute maximum of lines to be potentially written per 64 bytes
-	CMPQ AX, rows_len+40(FP)
-	JGE  done                // exit out and make sure more memory is allocated
-
-	MOVQ buf+0(FP), DI
-
-
-
-
-
+	JNZ  done                 // in case we end exactly on a 64-byte boundary, check if we need to add a delimiter
 
 addTrailingDelimiter:
 	// simulate a last "trailing" delimiter, but only
 	// if the buffer is not already terminated by a delimiter
-	MOVQ lastCharIsDelimiter+24(FP), CX
+	MOVQ lastCharIsDelimiter+80(FP), CX
 	CMPQ CX, $1
 	JZ   done
 
-	MOVQ input+104(FP), SI
+	MOVQ input2+64(FP), SI
 	MOVQ $1, CX            // first bit marks first char is delimiter
 	MOVQ CX, 8(SI)
 	MOVQ $0, CX
 	MOVQ CX, 0(SI)
 	MOVQ CX, 16(SI)
 
-	MOVQ offset+112(FP), DI
-	MOVQ output+120(FP), R9
+	MOVQ offset+56(FP), DI
+	MOVQ output2+72(FP), R9
 
 	PUSHQ DX
-	MOVQ  input+104(FP), DX
+	MOVQ  input2+64(FP), DX
 	CALL  ·stage2_parse(SB)
 	POPQ  DX
 
 done:
+exit:
 	VZEROUPPER
-	MOVQ DX, processed+128(FP)
+	MOVQ DX, processed+136(FP)
 	RET
