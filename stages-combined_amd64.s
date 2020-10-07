b45a4afa68f5f6cc693f9f7a2ae5c68f0b6966d0
frankw <fwessels@xs4all.nl>
2020-10-07 14:56:45 -0700

    Take out the combined version

--- stages-combined_amd64.s
+++
@@ -1,5 +0,0 @@
-//+build !noasm !appengine
-
-// stages_combined_buffer(buf []byte, separatorChar uint64, input1 *stage1Input, output1 *stage1Output, postProc *[]uint64, offset uint64, input2 *Input, output2 *OutputAsm, lastCharIsDelimiter uint64, rows []uint64, columns []string) (processed uint64)
-TEXT ·stages_combined_buffer(SB), 7, $0
-    RET
